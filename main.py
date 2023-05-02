6203130da8f950122d944a032a0898c1c837e2af
serverterem <nogradi.attila@diak.szbi-pg.hu>
2023-05-02 21:20:13 +0200

    játék mehanika, pontozás

--- main.py
+++ main.py
@@ -111,4 +111,46 @@ class Tetris:
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
         self.current_piece = self.new_piece()
         self.game_over = False
-        self.score = 0  
+        self.score = 0 
+
+    def new_piece(self):
+        shape = random.choice(SHAPES)
+        return Tetromino(self.width // 2, 0, shape)
+    
+    def valid_move(self, piece, x, y, rotation):
+        for i, row in enumerate(piece.shape[(piece.rotation + rotation) % len(piece.shape)]):
+            for j, cell in enumerate(row):
+                try:
+                    if cell == 'O' and (self.grid[piece.y + i + y][piece.x + j + x] != 0):
+                        return False
+                except IndexError:
+                    return False
+        return True
+    
+    def clear_lines(self):
+        lines_cleared = 0
+        for i, row in enumerate(self.grid[:-1]):
+            if all(cell != 0 for cell in row):
+                lines_cleared += 1
+                del self.grid[i]
+                self.grid.insert(0, [0 for _ in range(self.width)])
+        return lines_cleared
+    
+    def lock_piece(self, piece):
+        for i, row in enumerate(piece.shape[piece.rotation % len(piece.shape)]):
+            for j, cell in enumerate(row):
+                if cell == 'O':
+                    self.grid[piece.y + i][piece.x + j] = piece.color
+        lines_cleared = self.clear_lines()
+        self.score += lines_cleared * 100  
+        self.current_piece = self.new_piece()
+        if not self.valid_move(self.current_piece, 0, 0, 0):
+            self.game_over = True
+        return lines_cleared
+    
+    def update(self):
+        if not self.game_over:
+            if self.valid_move(self.current_piece, 0, 1, 0):
+                self.current_piece.y += 1
+            else:
+                self.lock_piece(self.current_piece)
