4588960f17dbbcd1f63988ab9739d2eae16195a5
serverterem <kiraly.bence.viktor@diak.szbi-pg.hu>
2023-05-02 22:17:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -154,3 +154,72 @@ class Tetris:
                 self.current_piece.y += 1
             else:
                 self.lock_piece(self.current_piece)
+
+    def draw(self, screen):
+        """Draw the grid and the current piece"""
+        for y, row in enumerate(self.grid):
+            for x, cell in enumerate(row):
+                if cell:
+                    pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
+
+        if self.current_piece:
+            for i, row in enumerate(self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
+                for j, cell in enumerate(row):
+                    if cell == 'O':
+                        pygame.draw.rect(screen, self.current_piece.color, ((self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
+
+def draw_score(screen, score, x, y):
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Score: {score}", True, WHITE)
+    screen.blit(text, (x, y))
+    
+    
+def draw_game_over(screen, x, y):
+    font = pygame.font.Font(None, 48)
+    text = font.render("Game Over", True, RED)
+    screen.blit(text, (x, y))
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Tetris')
+    clock = pygame.time.Clock()
+    game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
+    fall_time = 0
+    fall_speed = 50  
+
+    while True:
+        screen.fill(BLACK) 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if game.valid_move(game.current_piece, -1, 0, 0):
+                        game.current_piece.x -= 1 
+                if event.key == pygame.K_RIGHT:
+                    if game.valid_move(game.current_piece, 1, 0, 0):
+                        game.current_piece.x += 1 
+                if event.key == pygame.K_DOWN:
+                    if game.valid_move(game.current_piece, 0, 1, 0):
+                        game.current_piece.y += 1 
+                if event.key == pygame.K_UP:
+                    if game.valid_move(game.current_piece, 0, 0, 1):
+                        game.current_piece.rotation += 1
+                if event.key == pygame.K_SPACE:
+                    while game.valid_move(game.current_piece, 0, 1, 0):
+                        game.current_piece.y += 1 
+                    game.lock_piece(game.current_piece) 
+        delta_time = clock.get_rawtime() 
+        fall_time += delta_time 
+        if fall_time >= fall_speed:
+            game.update()
+            fall_time = 0
+        draw_score(screen, game.score, 10, 10)
+        game.draw(screen)
+        if game.game_over:
+            draw_game_over(screen, WIDTH // 2 - 100, HEIGHT // 2 - 30)
+            if event.type == pygame.KEYDOWN:
+                game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
+        pygame.display.flip()
+        clock.tick(60)
